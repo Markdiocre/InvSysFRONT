64e1b31894a3f5e3ca95aa22169d71e9e3b5ff64
Markdiocre <mark.manuel.bsuiness@gmail.com>
2022-06-01 16:57:32 +0800

    pwa ongoing

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import axios from 'axios'
 
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+})
+
 
 const app = createApp(App);
-axios.defaults.baseURL = 'https://invsys-api.herokuapp.com/';
-// axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+// axios.defaults.baseURL = 'https://invsys-api.herokuapp.com/';
+axios.defaults.baseURL = 'http://127.0.0.1:8000/';
 
 app.use(createPinia());
 app.use(router);
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,42 @@
 import { fileURLToPath, URL } from "url";
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
+      manifest: {
+        name: 'Inventory Management System',
+        short_name: 'IMS',
+        description: 'Inventory Management System',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      },
+      workbox: {
+        cleanupOutdatedCaches: false ,
+        sourcemap: true
+      }
+  })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
